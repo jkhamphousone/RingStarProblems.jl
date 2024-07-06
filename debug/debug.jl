bf51c2fa70bbca39660f4e370078f0e29561185b
jkhamphousone <research.khamphousone@gmail.com>
2024-07-07 00:11:15 +0200

    Adding debug.jl

---
+++ debug/debug.jl
@@ -0,0 +1,10 @@
+function log_assert(filename, left_hs, operator, right_hs, error_str="")
+    if !operator(left_hs, right_hs)
+        open("./debug/$(filename)_$(Dates.format(now(), "yyyy-mm-dd__HHhMM")).txt", "w") do file
+            write(file, "left hand side: $left_hs !$operator right hand side: $right_hs\n")
+            write(file, error_str)
+        end
+        @show left_hs, right_hs
+        # @assert operator(left_hs, right_hs)
+    end
+end
