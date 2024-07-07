2239b5ea6c0d4286a1435f8c96f808b53f6eb14d
jkhamphousone <research.khamphousone@gmail.com>
2024-07-07 02:47:52 +0200

    updating README.md

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ This is a my [julia](https://julialang.org/) project started during my PhD thesi
 The package can solve 1-R-RSP thanks to:
  - An Integer Linear Programming model
  - A Branch-and-Benders-cut algorithm
+
+Usage:
+```julia> pars = MainPar(solve_mod="Both",
+                      write_res="", 
+                      o_i="0", 
+                      s_ij="", 
+                      r_ij="", 
+                      backup_factor=0.01, 
+                      do_plot=false, 
+                      two_opt=0, 
+                      tildeV=100, 
+                      time_limit=120, 
+                      log_level=1, 
+                      sp_solve="poly", 
+                      redirect_stdio=false, 
+                      F=183, 
+                      html_user_notes=("seperate y_ij <= y_jj - x_ij on lazy constraints", "seperate y_ij <= y_jj - x_ij on lazy constraints"), 
+                      use_blossom=false, 
+                      alphas=[3], 
+                      nthreads=4,
+                      uc_strat=4)
+```
+Then:
+```julia>id_instance = 3
+   julia> main(pars, instance_id)
+```
--- src/RRSP.jl
+++ src/RRSP.jl
@@ -4,9 +4,12 @@ module RRSP
     @info "Loading Revise"
     using Revise
     using Aqua
-    using JET
+    @static if v"1.9" >= Base.VERSION >= v"1.7"
+        using JET
+    end
+        
     using JuMP
-    using Gurobiac
+    using Gurobi
     using MathOptInterface
     
     using Combinatorics, DelimitedFiles, Dates, Random
@@ -15,7 +18,7 @@ module RRSP
     using Graphs, GraphsFlows, GraphPlot, Plots
     
     using Parameters #https://stackoverflow.com/a/58022378/10094437
-    using Cairo, Suppressor, Fontconfig, Profile
+    using Cairo, Suppressor, Profile
     @info "Loading packages... [3/3] DONE"
     @info "Loading .jl files $(lpad("0%",4))"
     
@@ -48,36 +51,10 @@ module RRSP
     include("create_blossom_ineaqulities.jl")
     @info "Loading .jl files $(lpad("100%",4))"
     @info "took $(round(time() - a,digits=1))s to load packages and .jl files"
-    include("instance.jl")
-    include("solution.jl")
-    include("with_kw_mutable_structures.jl")
-    include("solution_checker.jl")
-    @info "Loading .jl files $(lpad("25%",4))"
-    include("create_subtour_constraint.jl")
-    # include("ilp_rho_rsp_st_chains.jl")
-    include("ilp_rrsp.jl")
-    include("benders_rrsp.jl")
-    include("benders_subproblem_poly.jl")
-    @info "Loading .jl files $(lpad("50%",4))"
-    include("benders_subproblem_ilp_primal.jl")
-    include("benders_subproblem_ilp_dual.jl")
-    include("print.jl")
-    include("three_four_rho_rsp.jl")
-    @info "Loading .jl files $(lpad("75%",4))"
-    include("utilities.jl")
-    # include("rho_rsp_lb.jl")
-    include("./plots/plots.jl")
-    include("local_searches.jl")
-    include("explore_F.jl")
-    include("main.jl")
-    include("post_optimization.jl")
-    include("read.jl")
-    include("../debug/debug.jl")
-    include("create_blossom_ineaqulities.jl")
+    
 
     @info "Loading .jl files $(lpad("100%",4))"
     export MainPar, main, t
-    @info time()-a
 
     # pars = MainPar(solve_mod="Both", alphas=[parse(Int, ARGS[2])], time_limit=60*60, write_res="html", log_level=1, plot_run=true, html_usernotes=ARGS[3])
     # main(pars, parse(Int, ARGS[1]))
